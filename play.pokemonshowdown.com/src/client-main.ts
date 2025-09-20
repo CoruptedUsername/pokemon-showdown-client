a0afa821ba0ae1501521594977f5fd8f1f4d053c
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-09-20 14:16:36 -0500

    Cottonee Cove Client v0.0.7e - Trying to fix logout

--- play.pokemonshowdown.com/src/client-main.ts
+++ play.pokemonshowdown.com/src/client-main.ts
@@ -2893,7 +2893,7 @@ export const OfficialAuth = new class {
 		// We can ignore the token access check, because it should revoke all things for this client_id anyways.
 		const response = await fetch(this.requestUrl("api/revoke"), {
 			method: "POST",
-			// mode: 'no-cors',
+			mode: 'no-cors',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 			},
