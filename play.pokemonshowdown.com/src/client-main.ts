a04ad68e4ed6d1a847b80921d2c498407a188de7
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-09-20 14:22:37 -0500

    Cottonee Cove Client v0.0.7f - Trying to fix logout

--- play.pokemonshowdown.com/src/client-main.ts
+++ play.pokemonshowdown.com/src/client-main.ts
@@ -717,17 +717,22 @@ export class PSUser extends PSStreamModel<PSLoginState | null> {
 		}
 	}
 	logOut() {
-		OfficialAuth.revoke().then(() => {
-			console.log('Finished revoke')
-			// PS.send(`/logout`);
-			PS.alert("You have been logged out.\n\nIf you wanted to change your name while staying connected, use the 'Change Name' button or the '/nick' command.");
-			this.name = "";
-			this.group = '';
-			this.userid = "" as ID;
-			this.named = false;
-			this.registered = null;
-			this.update(null);
-		}).catch(() => {PS.alert("Error logging out: Failed to revoke auth access."); } );
+		OfficialAuth.revoke().then(
+			() => {
+				console.log('Finished revoke')
+				// PS.send(`/logout`);
+				PS.alert("You have been logged out.\n\nIf you wanted to change your name while staying connected, use the 'Change Name' button or the '/nick' command.");
+				this.name = "";
+				this.group = '';
+				this.userid = "" as ID;
+				this.named = false;
+				this.registered = null;
+				this.update(null);
+			},
+			() => {
+				PS.alert("Error logging out: Failed to revoke auth access.")
+			}
+		);
 	}
 
 	updateRegExp() {
