744e04111b1530f69bf22add3d1aab00b67cf866
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-04-02 18:52:35 -0500

    Cottonee Cove Client v0.2.0 Pre-Release 4.9 - Moving sprite override Location

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -1113,7 +1113,7 @@ export class BattleScene implements BattleSceneStub {
 
 	addPokemonSprite(pokemon: Pokemon) {
 		console.log(`1115, ${this}`);
-		throw("erg");
+		throw new Error("Erg");
 		const sprite = new PokemonSprite(Dex.getSpriteData(pokemon, pokemon.side.isFar, {
 			gen: this.gen,
 			mod: this.mod,
