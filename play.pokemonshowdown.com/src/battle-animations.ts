e8a9ceb08d20f40cdfd7f8a426341705939fd166
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-05-07 01:03:06 -0500

    Cottonee Cove Client v0.3.0.2j - WE LOVE PRINT STATEMENTS (Reprise, cont. pt 2)

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -704,11 +704,11 @@ export class BattleScene implements BattleSceneStub {
 				// in VGC (bring 6 pick 4) and other pick-less-than-you-bring formats, this is
 				// a pokemon that's been brought but not necessarily picked
 				const details = this.getDetailsText(poke);
-				console.log(side.battle.dex.modid);
+				console.log(window.BattleTeambuilderTable.formats[toID(this.battle.tier)].mod);
 				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, this.battle.dex.modid)};opacity:0.6" aria-label="${details}"></span>`;
 			} else {
 				const details = this.getDetailsText(poke);
-				console.log(this.mod);
+				console.log(window.BattleTeambuilderTable.formats[toID(this.battle.tier)].mod);
 				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, this.battle.dex.modid)}" aria-label="${details}"></span>`;
 			}
 			if (i % 3 === 2) pokemonhtml += `</div><div class="teamicons">`;
