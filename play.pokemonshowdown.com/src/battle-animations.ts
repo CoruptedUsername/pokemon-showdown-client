4840d534b141f502e4078c884d7919a74ccd6b4d
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2026-05-07 01:06:42 -0500

    Cottonee Cove Client v0.3.0.2k - God please say this is it

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -704,12 +704,10 @@ export class BattleScene implements BattleSceneStub {
 				// in VGC (bring 6 pick 4) and other pick-less-than-you-bring formats, this is
 				// a pokemon that's been brought but not necessarily picked
 				const details = this.getDetailsText(poke);
-				console.log(window.BattleTeambuilderTable.formats[toID(this.battle.tier)].mod);
-				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, this.battle.dex.modid)};opacity:0.6" aria-label="${details}"></span>`;
+				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, window.BattleTeambuilderTable.formats[toID(this.battle.tier)]?.mod)};opacity:0.6" aria-label="${details}"></span>`;
 			} else {
 				const details = this.getDetailsText(poke);
-				console.log(window.BattleTeambuilderTable.formats[toID(this.battle.tier)].mod);
-				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, this.battle.dex.modid)}" aria-label="${details}"></span>`;
+				pokemonhtml += `<span${tooltipCode} style="${Dex.getPokemonIcon(poke, !side.isFar, window.BattleTeambuilderTable.formats[toID(this.battle.tier)]?.mod)}" aria-label="${details}"></span>`;
 			}
 			if (i % 3 === 2) pokemonhtml += `</div><div class="teamicons">`;
 		}
